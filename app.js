f71717d40058cb16c6d113542d6d594d9b7af92f
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-03-07 17:57:32 +0100

    Přidání potřebných souborů

---
+++ app.js
@@ -0,0 +1,50 @@
+let qrData = document.getElementById("qr-data"),
+    qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: "https://www.alba-rosa.cz",
+        width: 200,
+        height: 200,
+        colorDark: "#0f1523",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    }),
+    generateButton = document.getElementById("generate-button");
+
+generateButton.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let data = qrData.value;
+    qrcode.makeCode(data);
+
+    if (data == "") {
+        alert("You Cannot Leave Fields Empty!");
+    }
+});
+
+//Downloading QR Code
+
+let downloadButton = document.getElementById("download-button"),
+    qrCanvas = document.querySelector("canvas"),
+    generatedQrCode = document.querySelector("img");
+
+downloadButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let data = qrData.value;
+
+    if (data == "") {
+        alert("Please Generate a QR Code To Download !");
+    } else {
+        const dataURI = qrCanvas.toDataURL("image / png");
+        generatedQrCode.src = dataURI;
+        // For Microsoft Edge or Old Browser Only
+        if (window.navigator.msSaveBlob) {
+            window.navigator.msSaveBlob(qrCanvas.msToBlob(), "qr-code.png")
+        } else {
+            const a = document.createElement("a");
+            document.body.appendChild(a);
+            a.href = qrCanvas.toDataURL();
+            a.download = "qr-code.png";
+            a.click();
+            document.body.removeChild(a);
+        }
+    }
+});
